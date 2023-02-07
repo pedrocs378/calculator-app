008b0c0d7d8d6baf59bd45b4851aa580f8147d36
Pedro <pedrocs378@gmail.com>
2023-02-07 13:30:29 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider, Heading } from '@chakra-ui/react'
+
+export function App() {
+  return (
+    <ChakraProvider>
+      <Heading>Calculator</Heading>
+    </ChakraProvider>
+  )
+}
